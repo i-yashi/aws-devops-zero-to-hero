d9f71243670365d4de5d8592e3e4371aa6ed795d
Yashi Gupta <96427363+i-yashi@users.noreply.github.com>
2024-07-05 23:08:08 +0530

    Update start_container.sh

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull yashi01/simple-python-flask-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 yashi01/simple-python-flask-app
